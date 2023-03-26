05ac977e40133be5fa20a13b79a2e82ca02e9a5e
FangYuan <61506177+FangYuan33@users.noreply.github.com>
2023-03-26 14:59:09 +0800

    服务端向客户端写数据

--- netty/book/客户端和服务端的双向通信/client/FirstClientHandler.java
+++ netty/book/客户端和服务端的双向通信/client/FirstClientHandler.java
@@ -14,6 +14,10 @@ import java.util.Date;
  * @since 2023-03-26 14:25:45
  */
 public class FirstClientHandler extends ChannelInboundHandlerAdapter {
+
+    /**
+     * 客户端连接服务端成功后会回调该方法
+     */
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
         System.out.println(new Date() + ": 客户端写出数据");
@@ -35,4 +39,13 @@ public class FirstClientHandler extends ChannelInboundHandlerAdapter {
         return buffer;
     }
 
+    /**
+     * 收到服务端数据后，会回调该方法
+     */
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf byteBuf = (ByteBuf) msg;
+
+        System.out.println(new Date() + ": 客户端读到数据 -> " + byteBuf.toString(StandardCharsets.UTF_8));
+    }
 }
--- netty/book/客户端和服务端的双向通信/server/FirstServerHandler.java
+++ netty/book/客户端和服务端的双向通信/server/FirstServerHandler.java
@@ -16,12 +16,27 @@ import java.util.Date;
 public class FirstServerHandler extends ChannelInboundHandlerAdapter {
 
     /**
-     * 读取从客户端发送过来的信息
+     * 收到客户端数据后会回调该方法
      */
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         ByteBuf byteBuf = (ByteBuf) msg;
 
         System.out.println(new Date() + ": 服务端读到数据 -> " + byteBuf.toString(StandardCharsets.UTF_8));
+
+        // 回复客户端数据
+        System.out.println(new Date() + ": 服务端写出数据");
+        ByteBuf out = getByteBuf(ctx);
+        ctx.channel().writeAndFlush(out);
+    }
+
+    private ByteBuf getByteBuf(ChannelHandlerContext ctx) {
+        ByteBuf buffer = ctx.alloc().buffer();
+
+        byte[] bytes = "你好哇".getBytes(StandardCharsets.UTF_8);
+
+        buffer.writeBytes(bytes);
+
+        return buffer;
     }
 }
